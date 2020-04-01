2d72b11862c5b9340d4f99f901cae7f9addf5876
Hangning Zhang <zhang521147@gmail.com>
2020-04-02 01:26:02 +0800

    Sorry for the huge commit....
    1.Added another 3rd person controller plugin
    
    2.Implemented the shadow solution manager
    -No shadow
    -Blob shadow
    -Soft shadow
    -Hard shadow
    
    3.Implemented the UI to use the shadow manager

---
+++ 3DPlatformer_Prototype/Assets/ButtonEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public ShadowManager m_shadowManager;
+    
+    //The Onclick of button doesn't take enum, which I have to use the extra step....
+    public void ChangeShadowType(string p_type)
+    {
+        switch (p_type)
+        {
+            case "NOSHADOW":   { m_shadowManager.ChangeShadowType(SHADOWTYPE.NOSHADOW); break; }
+            case "SOFTSHADOW": { m_shadowManager.ChangeShadowType(SHADOWTYPE.SOFTSHADOW); break; }
+            case "HARDSHADOW": { m_shadowManager.ChangeShadowType(SHADOWTYPE.HARDSHADOW); break; }
+            case "BLOBSHADOW": { m_shadowManager.ChangeShadowType(SHADOWTYPE.BLOBSHADOW); break; }
+        }
+    }
+}
---
+++ 3DPlatformer_Prototype/Assets/ShadowManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SHADOWTYPE
+{
+    NOSHADOW,
+    SOFTSHADOW,
+    HARDSHADOW,
+    BLOBSHADOW
+}
+
+public class ShadowManager : MonoBehaviour
+{
+    public Light m_DirLight;        //Directional light
+    public GameObject m_blobShadow; // blob shadow
+
+    public SHADOWTYPE currentType;
+    
+    //Entrance method to change the shadow type
+    public void ChangeShadowType(SHADOWTYPE p_type)
+    {
+        if (currentType == p_type)    //if it's the same then dont do anything
+        {
+            return;
+        }
+
+        currentType = p_type;
+        
+        switch (p_type)
+        {
+            case SHADOWTYPE.NOSHADOW:   { ChangeToNoShadow();   break; }
+            case SHADOWTYPE.SOFTSHADOW: { ChangeToSoftShadow(); break; }
+            case SHADOWTYPE.HARDSHADOW: { ChangeToHardShadow(); break; }
+            case SHADOWTYPE.BLOBSHADOW: { ChangeToBlobShadow(); break; }
+        }
+    }
+
+    private void ChangeToNoShadow()
+    {
+        m_DirLight.shadows = LightShadows.None;
+        m_blobShadow.SetActive(false);
+    }
+    
+    private void ChangeToSoftShadow()
+    {
+        m_DirLight.shadows = LightShadows.Soft;
+        m_blobShadow.SetActive(false);
+    }
+    
+    private void ChangeToHardShadow()
+    {
+        m_DirLight.shadows = LightShadows.Hard;
+        m_blobShadow.SetActive(false);
+    }
+    
+    private void ChangeToBlobShadow()
+    {
+        m_DirLight.shadows = LightShadows.None;
+        m_blobShadow.SetActive(true);
+    }
+}
