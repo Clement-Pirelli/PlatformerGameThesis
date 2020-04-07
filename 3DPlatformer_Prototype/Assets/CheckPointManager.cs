0afe9d2358fca01fe306b1c1ad381555a2830754
Hangning Zhang <zhang521147@gmail.com>
2020-04-07 21:05:52 +0800

    1.Added the new check point system
    2.Adjusted the camera yaw limitation, distance and highest
    3.Made the cylinders bigger

---
+++ 3DPlatformer_Prototype/Assets/CheckPointManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    public static CheckPointManager instance;
+    
+    private Transform currentSpawnPoint;
+    
+    private Transform m_player;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void SetSpawnPoint(Transform p_spawnPoint)
+    {
+        currentSpawnPoint = p_spawnPoint;
+    }
+
+    public void ResetPlayerPosition()
+    {
+        if (m_player == null)
+        {
+            m_player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        
+        m_player.position = currentSpawnPoint.position;
+    }
+
+}
---
+++ 3DPlatformer_Prototype/Assets/CheckPointTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointTrigger : MonoBehaviour
+{
+    public Transform m_SpawnPoint; //Spawn point
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CheckPointManager.instance.SetSpawnPoint(m_SpawnPoint);
+        }
+    }
+}
